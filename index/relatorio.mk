5924b7d31875be1848ed1a6e4e84d68da81051c0
vitorguinomio <vitorguinomio@gmail.com>
2025-12-02 18:22:51 +0000

    Fazendo relatorio de presença

---
+++ index/relatorio.mk
@@ -0,0 +1,409 @@
+[Ana Luiza]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 11 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Arthur Gonçalo]
+Equipe (Atuação): Banco de dados
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 0 dias presentes - Status: Reprovado
+Novembro: 0 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Arthur moreira]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 9 dias presentes - Status: Reprovado
+Novembro: 9 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Bruno Alves da silva]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 2 dias presentes - Status: Reprovado
+Outubro: 1 dias presentes - Status: Reprovado
+Novembro: 1 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Carlos Eduardo]
+Equipe (Atuação): Banco de dados
+
+Setembro: 10 dias presentes - Status: Reprovado
+Outubro: 5 dias presentes - Status: Reprovado
+Novembro: 0 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Cauã Lima]
+Equipe (Atuação): Banco de dados
+
+Setembro: 8 dias presentes - Status: Reprovado
+Outubro: 3 dias presentes - Status: Reprovado
+Novembro: 2 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Dalton de avelar]
+Equipe (Atuação): Banco de dados
+
+Setembro: 13 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 2 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Davi Morais]
+Equipe (Atuação): documentação
+
+Setembro: 6 dias presentes - Status: Reprovado
+Outubro: 7 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Emerson]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Enzo vieira]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Erick borba]
+Equipe (Atuação): documentação
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 10 dias presentes - Status: Reprovado
+Novembro: 6 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Felipe Augusto]
+Equipe (Atuação): Não Identificado
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 0 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Felipe Marcedo AEB]
+Equipe (Atuação): Não Identificado
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 0 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Felipe Shoji]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 14 dias presentes - Status: Aprovado (35h)
+Novembro: 6 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Filipe]
+Equipe (Atuação): Banco de dados
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 6 dias presentes - Status: Reprovado
+Novembro: 7 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Guilherme Braga AEB]
+Equipe (Atuação): Não Identificado
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 0 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[João Omati]
+Equipe (Atuação): DevOps
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[João Victor]
+Equipe (Atuação): Banco de dados
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 6 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[João gabriel]
+Equipe (Atuação): documentação
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[João kevyn]
+Equipe (Atuação): documentação
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 7 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Juan Felipe]
+Equipe (Atuação): documentação
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Kaleu Back]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 14 dias presentes - Status: Aprovado (35h)
+Novembro: 6 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Kamilly]
+Equipe (Atuação): documentação
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Lucas Rodrigo]
+Equipe (Atuação): Não Identificado
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 0 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Luís gustavo]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 14 dias presentes - Status: Aprovado (35h)
+Novembro: 6 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Luís henrique vieira holanda]
+Equipe (Atuação): Banco de dados
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 2 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Mateus Recalde]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 0 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Matheus]
+Equipe (Atuação): documentação
+
+Setembro: 13 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 2 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Max muller]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Nicolas Matheus]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 1 dias presentes - Status: Reprovado
+Novembro: 1 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Nicolas]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 5 dias presentes - Status: Reprovado
+Novembro: 0 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Pedro de Oliveira]
+Equipe (Atuação): Não Identificado
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 0 dias presentes - Status: Reprovado
+Novembro: 4 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
+
+[Pedro torres]
+Equipe (Atuação): documentação
+
+Setembro: 13 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 2 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Rayane]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Sebastian]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Sthefanny Ramos]
+Equipe (Atuação): back-end
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 14 dias presentes - Status: Aprovado (35h)
+Novembro: 6 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Thauane]
+Equipe (Atuação): documentação
+
+Setembro: 0 dias presentes - Status: Reprovado
+Outubro: 13 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 35 horas
+Observação:
+__________________________________________________
+
+[Viceleno]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Vitor Sousa]
+Equipe (Atuação): Banco de dados
+
+Setembro: 12 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 8 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Yasmin]
+Equipe (Atuação): documentação
+
+Setembro: 13 dias presentes - Status: Aprovado (35h)
+Outubro: 12 dias presentes - Status: Aprovado (35h)
+Novembro: 2 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 70 horas
+Observação:
+__________________________________________________
+
+[Yuri]
+Equipe (Atuação): Front-end, UI/UX
+
+Setembro: 11 dias presentes - Status: Reprovado
+Outubro: 6 dias presentes - Status: Reprovado
+Novembro: 7 dias presentes - Status: Reprovado
+Total de Horas Complementares a Receber: 0 horas
+Observação:
+__________________________________________________
